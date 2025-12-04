ecbc12f75b5f3005a34eddc82abfc3ec2c84d9a2
Kimheng-SOK <sokkimheng008@gmail.com>
2025-12-05 00:03:33 +0700

    update aside bar route link, add features view in admin pages, add chart bar and line graph

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,7 @@
 <script lang="ts">
 import Footer from './components/Footer.vue'
 import NavBar from './components/NavBar.vue'
-import AdminSideBar from './components/layout/AdminSideBar.vue'
+import AdminSideBar from './components/layout/AdminASide.vue'
 
 export default {
   name: 'App',
---
+++ src/views/admin/ManageProduct.vue
@@ -0,0 +1,12 @@
+<template>
+  <div class="manage-products">
+    <!-- empty placeholder to avoid parser error -->
+  </div>
+</template>
+
+<script lang="ts">
+// Component logic goes here (kept empty to avoid errors)
+export default {
+  name: 'ManageProducts',
+}
+</script>
