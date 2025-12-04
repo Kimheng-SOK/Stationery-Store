8daa4fc4feae096f23856913d36d9347fc4da19f
PHE RITHIKA <pherithika@gmail.com>
2025-12-04 21:50:48 +0700

    'authentication'

---
+++ src/assets/assets.ts
@@ -0,0 +1,14 @@
+// src/assets/assets.ts
+import pfpImage from './image/pfp.png'
+import facebookImage from './image/facebook.png'
+import googleImage from './image/google.png'
+import bookImage from './image/book_details.png'
+
+export const assets = {
+  access: pfpImage,
+  facebook: facebookImage,
+  google: googleImage,
+  book_details: bookImage,
+}
+
+export default assets
--- src/router/index.ts
+++ src/router/index.ts
@@ -1,21 +1,21 @@
-import { createRouter, createWebHistory } from 'vue-router'
-//import HomeView from '../views/HomeView.vue'
+import ProductDetails from "@/components/ProductDetails.vue";
+import SignIn from "@/components/SignIn.vue";
+import SignUp from "@/components/SignUp.vue";
+import { createRouter, createWebHistory } from "vue-router";
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
+
   routes: [
-    // {
-    //   path: '/',
-    //   name: 'home',
-    //   component: { template: '<h1>Welcome to Home Page</h1>' }
-    // },
-    // {
-    //   path: '/about',
-    //   name: 'about',
-    //   component: { template: '<h1>Welcome to Home Page</h1>'}
-    // },
-
-  ],
+    {
+      path: '/signup' , component: SignUp
+    },
+    {
+      path: '/signin' , component: SignIn
+    },
+    {
+      path:'/productdetails', component: ProductDetails
+    },
+  ]
 })
-
 export default router
